c3a69e5e00da97bc7d9dfe34b38ffd6858677181
yurilsn <luizyuri57@gmail.com>
2023-09-08 14:18:58 -0300

    Adicionando arquivos com o projeto

--- src/Mega_sena.java
+++ src/Mega_sena.java
@@ -1,2 +1,22 @@
+import java.util.Random;
+
 public class Mega_sena {
+
+    public static void main(String[] args) {
+        int i = 0;
+        int number;
+        Random gerador = new Random();
+
+        while(i < 6){
+            number = gerador.nextInt(60);
+            System.out.println(number);
+            i++;
+        }
+//        for(int i = 0; i < 6; i++){
+//            number = gerador.nextInt(60);
+//            System.out.println(number);
+//        }
+
+    }
+
 }
